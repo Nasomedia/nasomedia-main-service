5e3731c9457081c6c36e253bc0151603795ee637
hwc9169 <hwc9169@gmail.com>
2021-11-08 10:16:50 +0900

    파일 업로드 기능 추가

--- app/api/v1/deps.py
+++ app/api/v1/deps.py
@@ -1,5 +1,9 @@
 from typing import Generator
+from fastapi import UploadFile
 from app.db.session import SessionLocal
+from app.core.config import settings
+from azure.storage.blob.aio import BlobServiceClient
+from azure.storage.blob import ContentSettings
 
 def get_db() -> Generator:
     try:
@@ -7,3 +11,19 @@ def get_db() -> Generator:
         yield db
     finally:
         db.close()
+
+class AzureBlobService():
+    connection_string = settings.AZURE_STORAGE_CONNECTION_STRING
+    container_name = settings.AZURE_STORAGE_CONTAINER_NAME
+    blob_url = settings.BLOB_URL
+        
+    async def upload_file(self, filename: str ,file_in: UploadFile):
+        blob_service_client = BlobServiceClient.from_connection_string(self.connection_string)
+    
+        async with blob_service_client:
+            container_client = blob_service_client.get_container_client(self.container_name)
+            await container_client.upload_blob(
+                name=filename,
+                data=file_in.file,
+                content_settings=ContentSettings(content_type=file_in.content_type)
+            )
