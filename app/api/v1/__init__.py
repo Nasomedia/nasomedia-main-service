14f637525a0249bf46ecb9a1501c44943a1a78d8
hwc9169 <hwc9169@gmail.com>
2021-11-08 10:14:32 +0900

    prefix 수정

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,9 +3,9 @@ from fastapi import APIRouter
 from app.api.v1.endpoints import author, episode, genre, publisher, tag, series
 
 api_router = APIRouter()
-api_router.include_router(author.router, prefix="/authors", tag=["author"])
-api_router.include_router(episode.router, prefix="/episodes", tag=["episode"])
-api_router.include_router(genre.router, prefix="/genres", tag=["genre"])
-api_router.include_router(publisher.router, prefix="/publishers", tag=["publisher"])
-api_router.include_router(tag.router, prefix="/tags", tag=["tag"])
-api_router.include_router(series.router, prefix="/series", tag=["series"])
+api_router.include_router(author.router, prefix="/authors", tags=["author"])
+api_router.include_router(episode.router, prefix="/episodes", tags=["episode"])
+api_router.include_router(genre.router, prefix="/genres", tags=["genre"])
+api_router.include_router(publisher.router, prefix="/publishers", tags=["publisher"])
+api_router.include_router(tag.router, prefix="/tags", tags=["tag"])
+api_router.include_router(series.router, prefix="/serieses", tags=["series"])
