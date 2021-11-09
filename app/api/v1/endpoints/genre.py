bcd7e744b1409e89a884a5b3b146c21cb35d538c
hwc9169 <hwc9169@gmail.com>
2021-11-09 14:13:47 +0900

    라우터 정렬

--- app/api/v1/endpoints/genre.py
+++ app/api/v1/endpoints/genre.py
@@ -10,16 +10,16 @@ from app.api.v1 import deps
 
 router = APIRouter()
 
-router.get("/", response_model=List[schemas.Genre])
+@router.get("/", response_model=List[schemas.Genre])
 def read_genre(
-    *,
     db: Session = Depends(deps.get_db),
+    *,
     keyword: Optional[str] = Query(
         "",
         title="검색 키워드"
     ),
 
-) -> Any:
+):
     """
     Retrieve author
     """
@@ -29,45 +29,45 @@ def read_genre(
     return genres
 
 
-router.post("/", response_model=schemas.Genre)
+@router.post("/", response_model=schemas.Genre)
 def create_genre(
-    *,
     db: Session = Depends(deps.get_db),
+    *,
     genre_in: schemas.GenreCreate
-) -> Any:
+):
     """
     Create new author
     """
-    genre = crud.genre.create(db=db, obj_in=genre_in)
+    genre = crud.genre.create(db, obj_in=genre_in)
     return genre
 
 
-router.put("/{genre_id}", response_model=schemas.Genre)
-def update_author(
-    *,
+@router.put("/{genre_id}", response_model=schemas.Genre)
+def update_genre(
     db: Session = Depends(deps.get_db),
+    *,
     genre_id: int,
     genre_in: schemas.GenreUpdate
 ):
     """
     Update Genre
     """
-    genre = crud.genre.get(db=db, id=genre_id)
+    genre = crud.genre.get(db, id=genre_id)
     if not genre:
         raise HTTPException(status_code=404, detail="Genre not found")
-    genre = crud.genre.update(db=db, obj_in=genre_in)
+    genre = crud.genre.update(db, db_obj=genre, obj_in=genre_in)
     return genre
 
 
-router.delete("/{genre_id}", response_model=schemas.Genre)
+@router.delete("/{genre_id}", response_model=schemas.Genre)
 def delete_genre(
-    *,
     db: Session = Depends(deps.get_db),
+    *,
     genre_id: int,
 ):
     "Delete Genre"
-    genre = crud.genre.get(db=db, id=genre_id)
+    genre = crud.genre.get(db, id=genre_id)
     if not genre:
         raise HTTPException(status_code=404, detail="Genre not found")
-    genre = crud.genre.delete(db=db, id=genre_id)
+    genre = crud.genre.delete(db, id=genre_id)
     return genre
