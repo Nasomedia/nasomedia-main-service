00df686c5b01fe3d9acb8a9239187c24625d2cd9
hwc9169 <hwc9169@gmail.com>
2021-11-09 09:02:32 +0900

    내림차순 기능 추가

--- app/crud/series.py
+++ app/crud/series.py
@@ -11,6 +11,8 @@ from app.schemas.sort_enum import  SeriesSortEnum
 
 from .utils import get_kst_now, sync_update_date
 
+import sqlalchemy
+
 class CRUDSeries(CRUDBase[Series, SeriesCreate, SeriesUpdate]):
     def get_multi(
         self, 
@@ -18,21 +20,23 @@ class CRUDSeries(CRUDBase[Series, SeriesCreate, SeriesUpdate]):
         *,
         sort_by: SeriesSortEnum = "id",
         keyword: Optional[str] = None,
+        desc: bool = True,
         **kwargs
     ) -> List[Series]:
+        direction = sqlalchemy.desc if desc else sqlalchemy.asc
         if keyword:
             return db.query(self.model)\
                 .filter(Series.title.like(f"%{keyword}%"))\
                 .filter_by(**kwargs)\
-                .order_by(text(f"{sort_by}")).all()
+                .order_by(direction(getattr(Series, sort_by))).all()
     
         return db.query(self.model)\
             .filter_by(**kwargs)\
-            .order_by(text(f"{sort_by}")).all()
+            .order_by(direction(getattr(Series, sort_by))).all()
     
     def create(self, db: Session, *, obj_in: SeriesCreate) -> Series:
         obj_in_data = jsonable_encoder(obj_in)
-        
+
         db_obj = self.model(**obj_in_data)
         db_obj.created_at = get_kst_now()
         db.add(db_obj)
