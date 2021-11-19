f129db5426cc0a8587d421b7d3258c537c092dc0
hwc9169 <hwc9169@gmail.com>
2021-11-19 18:53:44 +0900

    오타 수정

--- alembic/env.py
+++ alembic/env.py
@@ -1,20 +1,16 @@
 from __future__ import with_statement
+from logging.config import fileConfig
+from sqlalchemy import pool
+from sqlalchemy import engine_from_config
+from alembic import context
+import sys
 
 import os
 import dotenv
 
 dotenv.load_dotenv()
 
-import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-
-
-from alembic import context
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-from logging.config import fileConfig
-
-
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -36,12 +32,14 @@ target_metadata = Base.metadata
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
+
 def get_url():
     user = os.getenv("POSTGRES_USER", "postgres")
     password = os.getenv("POSTGRES_PASSWORD", "")
     server = os.getenv("POSTGRES_SERVER", "db")
     db = os.getenv("POSTGRES_DB", "app")
     return f"postgresql://{user}:{password}@{server}/{db}"
+
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
@@ -77,8 +75,8 @@ def run_migrations_online():
     configuration = config.get_section(config.config_ini_section)
     configuration["sqlalchemy.url"] = get_url()
     connectable = engine_from_config(
-        configuration, 
-        prefix="sqlalchemy",
+        configuration,
+        prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
