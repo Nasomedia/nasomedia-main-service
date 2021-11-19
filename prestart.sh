7d0af6a65ea0a1d65709d3eed3a05c503e4766ee
hwc9169 <hwc9169@gmail.com>
2021-11-19 20:31:27 +0900

    오타 수정

--- prestart.sh
+++ prestart.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 # Let the DB start
-python /app/script/backend_pre_start.py
+python app/script/backend_pre_start.py
 
 # Run migrations
 alembic upgrade head
 
 # Create initial data in DB
-python /app/script/initial_data.py
+python app/script/initial_data.py
