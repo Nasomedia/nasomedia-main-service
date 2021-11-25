33405d01f1a664c7a033185f1f519c0ed089c539
hwc9169 <hwc9169@gmail.com>
2021-11-25 17:12:46 +0900

    CORS 오류 수정

--- app/core/config.py
+++ app/core/config.py
@@ -10,7 +10,16 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/v1"
     PROJECT_NAME: str = os.getenv("PROJECT_NAME")
 
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = json.loads(
+        os.getenv("BACKEND_CORS_ORIGINS"))
+
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
 
     POSTGRES_USER: str = os.getenv("POSTGRES_USER")
     POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASWORD")
