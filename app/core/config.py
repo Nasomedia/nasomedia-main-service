bb54650270da6dab2e512107d9986dd856dcd980
hwc9169 <hwc9169@gmail.com>
2021-12-06 19:56:15 +0900

    도커 이미지 업데이트

--- app/core/config.py
+++ app/core/config.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings(BaseSettings):
-    API_V1_STR: str = "/v1"
+    API_V1_STR: str = os.getenv("API_V1_STR","/api/v1")
     PROJECT_NAME: str = os.getenv("PROJECT_NAME")
 
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = json.loads(
